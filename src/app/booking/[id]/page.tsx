ff8a76bfdbcbd3bc6e28e709013fe2e85a252720
Loadszz <loadszz94@gmail.com>
2025-10-14 22:25:32 +0300

    push

--- src/app/booking/[id]/page.tsx
+++ src/app/booking/[id]/page.tsx
@@ -2,17 +2,19 @@ import BookingPageClient from '@/components/sections/booking/BookingPageClient'
 import { getProducts } from '@/lib/getProducts'
 import { redirect } from 'next/navigation'
 
-type PageParams = { id: string }
-
-const Page = async ({ params }: { params: Promise<PageParams> }) => {
-	// Ожидаем params с помощью await
-	const resolvedParams = await params
+type PageParams = {
+	id: string
+}
+const page = async ({ params }: { params: Promise<PageParams> }) => {
 	const products = await getProducts()
-	const product = products.find(p => p.id === Number(resolvedParams.id))
+	const { id } = await params
+	const product = products.find(p => p.id === Number(id))
 
-	if (!product) redirect('/booking')
+	if (!product) {
+		redirect('/booking')
+	}
 
 	return <BookingPageClient product={product} />
 }
 
-export default Page
+export default page
--- src/lib/getProducts.ts
+++ src/lib/getProducts.ts
@@ -1,9 +1,17 @@
 import { Product } from '@/data/products.type'
+
 export async function getProducts(): Promise<Product[]> {
-	const res = await fetch('http://localhost:3000/api/products', {
+	// Для SSR на сервере: Vercel или локальный сервер
+	const baseUrl = process.env.VERCEL_URL // если на Vercel
+		? `https://${process.env.VERCEL_URL}`
+		: 'http://localhost:3000' // локально
+
+	const res = await fetch(`${baseUrl}/api/products`, {
 		cache: 'no-store', // всегда свежие данные
 	})
+
 	if (!res.ok) throw new Error('Не удалось загрузить товары')
+
 	const data = await res.json()
 	return data.products as Product[]
 }
