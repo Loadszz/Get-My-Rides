7fa6bf951727591c5afa2b630db081f7493c0ded
Loadszz <loadszz94@gmail.com>
2025-10-15 00:39:44 +0300

    Update 9 fix 7

--- src/app/booking/[id]/page.tsx
+++ src/app/booking/[id]/page.tsx
@@ -1,20 +1,20 @@
 import BookingPageClient from '@/components/sections/booking/BookingPageClient'
-import { getProducts } from '@/lib/getProducts'
+import { getProduct } from '@/lib/getProducts'
 import { redirect } from 'next/navigation'
 
 type PageParams = {
 	id: string
 }
-const page = async ({ params }: { params: Promise<PageParams> }) => {
-	const products = await getProducts()
-	const { id } = await params
-	const product = products.find(p => p.id === Number(id))
 
-	if (!product) {
+const page = async ({ params }: { params: Promise<PageParams> }) => {
+	const { id } = await params // развернули Promise
+	let product
+	try {
+		product = await getProduct(id)
+	} catch {
 		redirect('/booking')
 	}
 
 	return <BookingPageClient product={product} />
 }
-
 export default page
